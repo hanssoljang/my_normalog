74ebe5143f58555c614205235294dffeb96343d1
hansol <hansol.jang@gmail.com>
2020-02-13 09:04:12 +0900

    update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `가장 보통의 존재`,
+  description: `보통사람`,
+  author: `[Evergreen]`,
+  introduction: `멈추지 않는 삶 속에서 행복과 즐거움을 찾아가는 수행자 장한솔입니다. 
+  고객에게 가치있는 경험을 제공하기 위해, 끊임없이 배우고 다양한 시도를 하고 있습니다. 
+  고객을 행복하게 하기 위해, 나부터 행복하고 함께 제품을 만드는 사람을 행복하게 하는 것부터 시작합니다. `,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    github: `hanssoljang`, // Your GitHub account
+    medium: `@hsjcompany`, // Your brunch account
+    twitter: `hsjcompany/`, // Your Instagram account
+    facebook: `hanssoljang`, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
